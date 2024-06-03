4f0bcabeb09d7572a43abec4c226abe7c770d459
Josh <alejandromzv18@gmail.com>
2024-06-03 17:24:57 -0400

    'urlfix'

--- src/components/FormGenerator.vue
+++ src/components/FormGenerator.vue
@@ -273,7 +273,7 @@ export default {
 
       console.log(data)
 
-      axios.post('https://inovanexgenerator.onrender.com/create', data)
+      axios.post('https://player-radio.inovanex.com/create', data)
       .then( function (res){
         if(res.status == 200){
           const resName = res.data.name.replace(/\s/g, '')
